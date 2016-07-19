4137d8688f754fbacc4ddedf0a368b0bc22a7611
Alex Ongarato <alexongarato@gmail.com>
2016-07-18 23:05:37 -0300

    tela contato incompleta

---
+++ public/scripts/Main.js
@@ -0,0 +1,16 @@
+var Main = {
+	init:function()
+	{
+		console.info("Starting app...");
+
+		window.addEventListener("load", function () 
+		{
+			var loader = document.getElementById("loader");
+			loader.setAttribute("class", "hidden");
+
+			console.info("App is ready.");
+		});
+	}
+}
+
+Main.init();
