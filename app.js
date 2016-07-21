7d20546cde406a8867c3b6428bf53235d7b8c29a
Alex Ongarato <alexongarato@gmail.com>
2016-07-20 23:47:40 -0300

    color transitions

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var path = require('path');
-var api = require('./api');
 var app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/api', api);
 
 module.exports = app;
